0b0f1da4979b097a6766b6dbfdf6f99341ab1cde
BrianHGrant <bhgrant8@gmail.com>
2017-01-14 13:05:28 -0800

    Initial Commit

---
+++ appleAirship/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  appleAirship
+//
+//  Created by Brian Grant on 1/14/17.
+//  Copyright © 2017 BHGConcepts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ appleAirship/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  appleAirship
+//
+//  Created by Brian Grant on 1/14/17.
+//  Copyright © 2017 BHGConcepts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
